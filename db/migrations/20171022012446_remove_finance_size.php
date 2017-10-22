0f1f7a9ee792ef43aeda28634ebeaf1730b070ff
Andrew Alkema <andrew.alkema@d2l.com>
2017-10-21 21:29:46 -0400

    remove financial size

---
+++ db/migrations/20171022012446_remove_finance_size.php
@@ -0,0 +1,33 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MyNewMigration extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $table = $this->table('organization');
+        $table->removeColumn('FinancialSizeId')
+            ->save();
+
+        $this->dropTable('financial_size');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $table = $this->table('financial_size', ['id' => 'FinancialSizeId']);
+        $table->addColumn('FinancialSize', 'string', ['limit' => 100])
+              ->save();
+
+        $table = $this->table('organization');
+        $table->addColumn('FinancialSizeId', int, ['null' => true])
+              ->AddForeignKey('FinancialSizeId', 'financial_size', ['constraint' => 'FinancialSizeId'])
+              ->update();
+    }
+}
